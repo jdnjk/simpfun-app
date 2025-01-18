212fe092d319478209099ad2b0c87eec6967acd0
jdnjk <2394058778@qq.com>
2025-01-18 17:50:25 +0800

    修复BUG

--- app/src/main/java/cn/jdnjk/simpfun/Welcome.java
+++ app/src/main/java/cn/jdnjk/simpfun/Welcome.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
-    SharedPreferences sp1;
+    SharedPreferences sp3;
     TextView topBarInfo;
 
     @Override
@@ -15,13 +15,13 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         // 获取 SharedPreferences，用于读取保存的用户信息
-        sp1 = this.getSharedPreferences("userInfo", MODE_PRIVATE);
+        sp3 = this.getSharedPreferences("info", MODE_PRIVATE);
         topBarInfo = this.findViewById(R.id.topBarInfo);
-        String username = sp1.getString("username", "NaN");  // 获取用户名,获取不到就摆烂
+        String username = sp3.getString("username", "NaN");  // 获取用户名,获取不到就摆烂
         // 获取并格式化顶栏信息
-        int points = sp1.getInt("point", 0);  // 获取积分
-        int diamonds = sp1.getInt("diamond", 0);  // 获取钻石
-        int uid = sp1.getInt("id", 0);  // 获取 uid
+        int points = sp3.getInt("point", 0);  // 获取积分
+        int diamonds = sp3.getInt("diamond", 0);  // 获取钻石
+        int uid = sp3.getInt("uid", 0);  // 获取 uid
 
         // 设置顶栏信息
         topBarInfo.setText("用户名: " + username + " 积分: " + points + " 钻石: " + diamonds + " uid: " + uid);
